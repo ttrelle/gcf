461b71cd12a38063acc07ec57607a9e3a36bb39c
Tobias Trelle <tobias.trelle@codecentric.de>
2019-01-11 13:29:41 +0100

    Simple GKE example

--- ml-vim/addfacedata.py
+++ ml-vim/addfacedata.py
@@ -2,12 +2,17 @@ from PIL import Image, ImageDraw
 
 BBOX_COLOR = (255, 255, 255)
 MARKER_COLOR = (255, 0, 0)
+LINE_WIDTH = 3
 
 FEATURES = [
     # Mouth
-    ["LOWER_LIP", "MOUTH_LEFT", "UPPER_LIP", "MOUTH_RIGHT"],
+    (True, ["LOWER_LIP", "MOUTH_LEFT", "UPPER_LIP", "MOUTH_RIGHT"]),
     # Nose
-    ["NOSE_BOTTOM_LEFT", "NOSE_BOTTOM_CENTER", "NOSE_BOTTOM_RIGHT", "MIDPOINT_BETWEEN_EYES"]
+    (True, ["NOSE_BOTTOM_LEFT", "NOSE_BOTTOM_CENTER", "NOSE_BOTTOM_RIGHT", "MIDPOINT_BETWEEN_EYES"]),
+    # left eyebrow
+    (False, ["LEFT_OF_LEFT_EYEBROW", "LEFT_EYEBROW_UPPER_MIDPOINT", "RIGHT_OF_LEFT_EYEBROW"]),
+    # right eyebrow
+    (False, ["LEFT_OF_RIGHT_EYEBROW", "RIGHT_EYEBROW_UPPER_MIDPOINT", "RIGHT_OF_RIGHT_EYEBROW"])
 ]
 
 def processImage(name, data, raw = None):
@@ -15,8 +20,8 @@ def processImage(name, data, raw = None):
     if raw is None:
         img = Image.open(name)
     else:
-        print("Using raw buffer ...")
         img = Image.open(raw)
+        
     img = img.convert("RGB")
     draw = ImageDraw.Draw(img)
 
@@ -39,13 +44,13 @@ def processImage(name, data, raw = None):
 def drawRect(draw, rect):
     p1 = rect["vertices"][0]
     p2 = rect["vertices"][2]
-    draw.rectangle( xy = [(p1["x"],p1["y"]), (p2["x"],p2["y"])], outline = (255,255,255), width= 2)
+    draw.rectangle( xy = [(p1["x"],p1["y"]), (p2["x"],p2["y"])], outline = BBOX_COLOR, width= LINE_WIDTH)
 
 def drawLandmark(draw, lm, text = True):
     # print(lm)
     x = int(lm["position"]["x"])
     y = int(lm["position"]["y"])
-    draw.ellipse(xy=[(x-1,y-1),(x+2, y+1)], fill = MARKER_COLOR, width=2)
+    draw.ellipse(xy=[(x-1,y-1),(x+2, y+1)], fill = MARKER_COLOR, width=LINE_WIDTH)
     if text:
         draw.text(xy= (x+5,y+2), text = lm["type"], fill = BBOX_COLOR)
 
@@ -55,11 +60,13 @@ def getLandmarkPoint(landmarks, id):
             pos = lm["position"]
             return (pos["x"], pos["y"])
 
-def drawFeature(draw, feature, landmarks, closePoly = True):
+def drawFeature(draw, feature, landmarks):
+    closePoly, featureList = feature
     poly = []
-    for item in feature:
-        p = getLandmarkPoint(landmarks, item)
-        poly.append(p)
+    for item in featureList:
+        poly.append(getLandmarkPoint(landmarks, item))
+
     if closePoly:
-        poly.append(getLandmarkPoint(landmarks, feature[0]))
-    draw.line(xy=poly, fill=MARKER_COLOR, width=3)
+        poly.append(getLandmarkPoint(landmarks, featureList[0]))
+        
+    draw.line(xy=poly, fill=MARKER_COLOR, width=LINE_WIDTH)
